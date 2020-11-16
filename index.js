1cc3cf892a0545604f27852ee6885fe4a72b33ca
Dave Winniford <dwinniford@gmail.com>
2020-11-16 14:00:26 -0600

    Done

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function findMinAndRemove(array){
-
+    let min = array[0]
+    let minIndex = 0
+    for (let index = 0; index < array.length; index++) {
+        if(array[index] < min) {
+            min = array[index]
+            minIndex = index
+        }
+    }
+    return array.splice(minIndex, 1)[0]
 }
 
 function insertionSort(array){
-
+    let sortedArray = []
+    while (array.length > 0) {
+        sortedArray.push(findMinAndRemove(array))
+    }
+    return sortedArray
 }
